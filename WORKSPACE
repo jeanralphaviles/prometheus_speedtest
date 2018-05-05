8c43cac27230efe0e4a56c4d04de630dd09aa98b
Jean-Ralph Aviles <jeanralph.aviles@gmail.com>
2018-05-05 00:17:28 -0700

    Add pushing of speedtest results to Prometheus Pushgateway.

--- WORKSPACE
+++ WORKSPACE
@@ -1,7 +1,7 @@
 git_repository(
-  name = "io_bazel_rules_python",
-  remote = "https://github.com/bazelbuild/rules_python.git",
-  commit = "8b5d0683a7d878b28fffe464779c8a53659fc645",
+    name = "io_bazel_rules_python",
+    commit = "8b5d0683a7d878b28fffe464779c8a53659fc645",
+    remote = "https://github.com/bazelbuild/rules_python.git",
 )
 
 load("@io_bazel_rules_python//python:pip.bzl", "pip_import")
@@ -10,8 +10,8 @@ load("@io_bazel_rules_python//python:pip.bzl", "pip_repositories")
 pip_repositories()
 
 pip_import(
-	name = "pip_deps",
-	requirements = "//:requirements.txt",
+    name = "pip_deps",
+    requirements = "//:requirements.txt",
 )
 
 load("@pip_deps//:requirements.bzl", "pip_install")
--- prometheus_speedtest.py
+++ prometheus_speedtest.py
@@ -2,49 +2,79 @@
 
 import argparse
 
+import prometheus_client
 import speedtest
+
+PARSER = argparse.ArgumentParser(
+    description='Export speedtest metrics to Prometheus Pushgateway.')
+PARSER.add_argument('-s', '--source_address', metavar='ADDR', type=str)
+PARSER.add_argument('-t', '--timeout', metavar='SEC', default=10, type=int)
+PARSER.add_argument('-p', '--pushgateway', default='localhost:9091', type=str)
+PARSER.add_argument('-n', '--name', default='prometheus_speedtest', type=str)
+
 
 class PrometheusSpeedtest(object):
   """Enapsulates behavior performing and reporting results of speedtests."""
 
-  def __init__(self, source_address, timeout, *args, **kwargs):
+  def __init__(self, pushgateway, jobname, source_address=None, timeout=None,
+               *args, **kwargs):
     """Instantiates a PrometheusSpeedtest object.
 
     Args:
+      pushgateway: str - host:port of Prometheus Pushgateway.
+      jobname: str - jobname to report to Prometheus Pushgateway.
       source_address: str - optional network address to bind to.
         e.g. 192.168.1.1.
       timeout: int - optional timeout for speedtest in seconds.
     """
     super(PrometheusSpeedtest, self).__init__(*args, **kwargs)
+    self.pushgateway = pushgateway
+    self.jobname = jobname
     self.source_address = source_address
     self.timeout = timeout
 
-  def test(self):
+  def _test(self):
     """Performs speedtest, returns results.
 
     Returns:
+      speedtest.SpeedtestResults object.
     """
-    client = speedtest.Speedtest(
-        source_address=self.source_address, timeout=self.timeout)
+    client = speedtest.Speedtest(source_address=self.source_address,
+                                 timeout=self.timeout)
     client.get_best_server()
     client.download()
     client.upload()
-    return client.results.dict()
+    return client.results
 
-def parse_args():
-  """Parses and returns command-line flags."""
-  parser = argparse.ArgumentParser(
-      description='Export speedtest metrics to Prometheus Pushgateway.')
-  parser.add_argument('-a', '--source_address', metavar='ADDR', type=str)
-  parser.add_argument('-t', '--timeout', metavar='SEC', default=10, type=int)
-  return parser.parse_args()
+  def _push(self, results):
+    """Pushes metrics about a speedtest to the Prometheus Pushgateway.
+
+    Args:
+      results: speedtest.SpeedtestResults object.
+    """
+    registry = prometheus_client.CollectorRegistry()
+    download_bps = prometheus_client.Gauge(
+        'download_speed_bps', 'Download speed (bit/s)',
+        registry=registry)
+    download_bps.set(results.download)
+    upload_bps = prometheus_client.Gauge(
+        'upload_speed_bps', 'Upload speed (bit/s)', registry=registry)
+    upload_bps.set(results.upload)
+    prometheus_client.push_to_gateway(
+        self.pushgateway, job=self.jobname, registry=registry)
+
+  def report(self):
+    """Performs a speedtest and pushes metrics to Prometheus Pushgateway."""
+    results = self._test()
+    self._push(results)
 
 
 def main():
   """Entry point for prometheus_speedtest.py."""
-  args = parse_args()
-  tester = PrometheusSpeedtest(args.source_address, args.timeout)
-  print(tester.test())
+  flags = PARSER.parse_args()
+  tester = PrometheusSpeedtest(
+      flags.pushgateway, flags.name, flags.source_address, flags.timeout)
+  tester.report()
 
 
 if __name__ == '__main__':
