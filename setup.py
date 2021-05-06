8d54872cf450f5e784d5e32a96216d4bde7b7431
Jan Stürtz <stuertz@users.noreply.github.com>
2021-05-06 06:34:24 +0200

    use pre-commit (#42)
    * feat: use pre-commit tool and central requirements-dev.txt
    
    * feat: scan all py files with yapf (setup.py) and also run pre-commit
    
    * prometheus_speedtest: Merge updated packages in setup.py after #42.
    
    Co-authored-by: Jan Stürtz <jan.stuertz@contact-software.com>
    Co-authored-by: Jean-Ralph Aviles <jeanralph.aviles@gmail.com>
    Co-authored-by: Jean-Ralph Aviles <jeanralphaviles@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -18,41 +18,44 @@ def read_file(rel_path):
         return rel_file.read().strip()
 
 
-setup(name='prometheus_speedtest',
-      author='Jean-Ralph Aviles',
-      author_email='jeanralph.aviles+pypi@gmail.com',
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: Apache Software License',
-          'Operating System :: OS Independent',
-          'Topic :: System :: Monitoring',
-          'Topic :: System :: Networking :: Monitoring',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python',
-      ],
-      description=('Performs speedtest-cli tests and pushes metrics to '
-                   'Prometheus Pushgateway'),
-      entry_points={
-          'console_scripts': [
-              ('prometheus_speedtest='
-               'prometheus_speedtest.prometheus_speedtest:init'),
-          ],
-      },
-      include_package_data=True,
-      packages=find_packages(),
-      install_requires=[
-          'absl-py==0.12.0',
-          'mock==4.0.3',
-          'prometheus_client==0.10.1',
-          'speedtest-cli==2.1.3',
-      ],
-      keywords=['prometheus', 'monitoring', 'speedtest', 'speedtest.net'],
-      license='Apache License, Version 2.0',
-      long_description=read_file('README.md'),
-      long_description_content_type='text/markdown',
-      py_modules=['prometheus_speedtest'],
-      setup_requires=['setuptools>=38.6.0'],
-      url='https://github.com/jeanralphaviles/prometheus_speedtest',
-      version=version.VERSION)
+setup(
+    name="prometheus_speedtest",
+    author="Jean-Ralph Aviles",
+    author_email="jeanralph.aviles+pypi@gmail.com",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: System :: Monitoring",
+        "Topic :: System :: Networking :: Monitoring",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python",
+    ],
+    description=("Performs speedtest-cli tests and pushes metrics to "
+                 "Prometheus Pushgateway"),
+    entry_points={
+        "console_scripts": [
+            ("prometheus_speedtest="
+             "prometheus_speedtest.prometheus_speedtest:init"),
+        ],
+    },
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        "absl-py==0.12.0",
+        "mock==4.0.3",
+        "prometheus_client==0.10.1",
+        "speedtest-cli==2.1.3",
+    ],
+    keywords=["prometheus", "monitoring", "speedtest", "speedtest.net"],
+    license="Apache License, Version 2.0",
+    long_description=read_file("README.md"),
+    long_description_content_type="text/markdown",
+    py_modules=["prometheus_speedtest"],
+    setup_requires=["setuptools>=38.6.0"],
+    extras_require={"dev": read_file("requirements-dev.txt")},
+    url="https://github.com/jeanralphaviles/prometheus_speedtest",
+    version=version.VERSION,
+)
